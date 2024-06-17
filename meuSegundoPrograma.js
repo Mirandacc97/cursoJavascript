6cfb0a5560469ec3dedb22300110d1dfc5b1ea45
Felipe Miranda Lima <40402331+Mirandacc97@users.noreply.github.com>
2024-06-16 23:08:31 -0300

    000

---
+++ meuSegundoPrograma.js
@@ -0,0 +1,5 @@
+console.log("Meu segundo programa");
+const idade = 26;
+console.log("Minha idade eh: " + idade);
+console.log("Mas se eu somar com 2 no console ela fica: " + (idade + 2));
+console.log("Mas minha idade permanece inalterada: " + idade);
---
+++ meuTerceiroPrograma.js
@@ -0,0 +1,6 @@
+console.log("Meu terceiro programa");
+
+////////// B o a s   p r á t i c a s //////////
+//JS é case sensitive
+//Variáveis e nome de arquivos são camelCase
+//JS é bem flexível quando à declaração de variáveis, mas não abuse disso, defina uma convenção e a siga
